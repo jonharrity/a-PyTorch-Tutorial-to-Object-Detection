b020c4e2c7f56628b4161c05087368a2a19a520a
Jon <joharrity@gmail.com>
2020-04-18 07:39:26 -0400

    reduced memory usage for training and eval; training logs output to "mylog"

--- eval.py
+++ eval.py
@@ -9,8 +9,8 @@ pp = PrettyPrinter()
 # Parameters
 data_folder = './'
 keep_difficult = True  # difficult ground truth objects must always be considered in mAP calculation, because these objects DO exist!
-batch_size = 64
-workers = 4
+batch_size = 8
+workers = 0
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 checkpoint = './checkpoint_ssd300.pth.tar'
 
