38275d6f7602df00fd3aabe6e75f029a139f8fe1
Jon <joharrity@gmail.com>
2020-04-18 07:35:37 -0400

    patched model.py from github discussions

--- model.py
+++ model.py
@@ -481,26 +481,39 @@ class SSD300(nn.Module):
 
                 # A torch.uint8 (byte) tensor to keep track of which predicted boxes to suppress
                 # 1 implies suppress, 0 implies don't suppress
-                suppress = torch.zeros((n_above_min_score), dtype=torch.uint8).to(device)  # (n_qualified)
+                #suppress = torch.zeros((n_above_min_score), dtype=torch.uint8).to(device)  # (n_qualified)
+                suppress = torch.zeros((n_above_min_score)).bool().to(device)  # (n_qualified) 
 
                 # Consider each box in order of decreasing scores
                 for box in range(class_decoded_locs.size(0)):
                     # If this box is already marked for suppression
                     if suppress[box] == 1:
                         continue
+    
+
+                    #PATCH
+                    # code from:
+                    # https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Object-Detection/issues/24
 
                     # Suppress boxes whose overlaps (with this box) are greater than maximum overlap
                     # Find such boxes and update suppress indices
-                    suppress = torch.max(suppress, overlap[box] > max_overlap)
+                    # suppress = torch.max(suppress, overlap[box] > max_overlap)
+                    suppress = suppress | (overlap[box] > max_overlap)
                     # The max operation retains previously suppressed boxes, like an 'OR' operation
 
                     # Don't suppress this box, even though it has an overlap of 1 with itself
                     suppress[box] = 0
 
                 # Store only unsuppressed boxes for this class
-                image_boxes.append(class_decoded_locs[1 - suppress])
-                image_labels.append(torch.LongTensor((1 - suppress).sum().item() * [c]).to(device))
-                image_scores.append(class_scores[1 - suppress])
+                image_boxes.append(class_decoded_locs[~suppress])
+                image_labels.append(
+                    torch.LongTensor(
+                        (~suppress).sum().item() * [c]).to(device)
+                )
+                image_scores.append(class_scores[~suppress])
+                #image_boxes.append(class_decoded_locs[1 - suppress])
+                #image_labels.append(torch.LongTensor((1 - suppress).sum().item() * [c]).to(device))
+                #image_scores.append(class_scores[1 - suppress])
 
             # If no object in any class is found, store a placeholder for 'background'
             if len(image_boxes) == 0:
